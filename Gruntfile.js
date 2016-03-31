30906e82ad84f151f43b9f0f7a827d8d0e23c0a3
Wolfram Eberius <edrush@posteo.de>
2016-03-31 08:53:52 +0200

    Added configuations: npm, bower, grunt; samizdatco/arbor as first visualisation attempt

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+
+    // ===========================================================================
+    // CONFIGURE GRUNT ===========================================================
+    // ===========================================================================
+    grunt.initConfig({
+
+        // get the configuration info from package.json ----------------------------
+        // this way we can use things like name and version (pkg.name)
+        pkg: grunt.file.readJSON('package.json'),
+
+        // all of our configuration will go here
+        copy: {
+            build: {
+                files: [
+                    // includes files within path
+                    {expand: true, src: ['bower_components/samizdatco-arbor/build/tmpl/*.js'], dest: 'Resources/Public/js/samizdatco-arbor/', filter: 'isFile', flatten: true},
+                ],
+            },
+        }
+
+    });
+
+    // ===========================================================================
+    // LOAD GRUNT PLUGINS ========================================================
+    // ===========================================================================
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.registerTask('default', 'copy');
+
+};
